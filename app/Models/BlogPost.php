7b3c5ea67f93738266aaefa5b246c2b32220bba8
Arvin Gasco <arvingasco@Arvins-Air.lan>
2021-11-29 16:14:24 +0000

    softdeletes added

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = ['title', 'content',];
 
@@ -21,6 +23,10 @@ class BlogPost extends Model
 
         static::deleting(function (BlogPost $blogPost) {
            $blogPost->comment()->delete();
+        });
+
+        static::restoring(function (BlogPost $blogPost) {
+            $blogPost->comment()->restore();
         });
     }
 }
